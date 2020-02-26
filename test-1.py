8057d6b4cee0b21ad9c47db154a72827753a0998
shudipta <shudipta.sharma@pathao.com>
2020-02-26 19:16:37 +0600

    Add sample locust tests

--- test-1.py
+++ test-1.py
@@ -1,46 +1,54 @@
-# from locust import HttpLocust, TaskSet, task, between
-
-# class UserBehaviour(TaskSet):
-#     def on_start(self):
-#         """ on_start is called when a Locust start before any task is scheduled """
-#         self.login()
-
-#     def on_stop(self):
-#         """ on_stop is called when the TaskSet is stopping """
-#         self.logout()
-
-#     def login(self):
-#         self.client.post("/login", {"username":"ellen_key", "password":"education"})
-
-#     def logout(self):
-#         self.client.post("/logout", {"username":"ellen_key", "password":"education"})
-
-#     @task(2)
-#     def index(self):
-#         self.client.get("/")
-
-#     @task(1)
-#     def profile(self):
-#         self.client.get("/profile")
-
-# class WebsiteUser(HttpLocust):
-#     task_set = UserBehaviour
-#     wait_time = between(5, 9)
-
 from locust import HttpLocust, TaskSet, task, between
 
+
 class MyTaskSet(TaskSet):
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+        print("[on_start]", self.locust)
+
+    def on_stop(self):
+        """ on_stop is called when the TaskSet is stopping """
+        print("[on_stop]", self.locust)
+
     @task(2)
     def queue(self):
-        response = self.client.post("/v1/queue", {"user_id": 123,"cross_dispatch": 0,"order_id": "asd","initiated_at": "{{currentDate}}","status": "asdasd"})
-        print("[/v1/queue] Response status code:", response.status_code)
-        print("[/v1/queue] Response content:", response.text)
+        payload = {"user_id": 123, "cross_dispatch": 0, "order_id": "asd", "initiated_at": "2020-02-24T07:43:04+06:00", "status": "asdasd"}
+
+        with self.client.post(
+            "http://localhost:4200/v1/queue",
+            # headers,
+            json=payload,
+            catch_response=True
+        ) as response:
+            print("[/v1/queue] Response status code: '%d'" % response.status_code)
+            print("[/v1/queue] Response content: '%s'" % response.text)
+            print("[/v1/queue] Response content1: '%s'" % response.content)
+            if response.status_code != 201:
+                response.failure("[/v1/queue] status_code: Expect 201, Got " + response.status_code)
+            else:
+                print("[/v1/queue] hurray! success")
+                response.success()
 
     @task(1)
     def callback(self):
-        response = self.client.post("/callback", {"name":"Joe","email":"joe@labstack"})
-        print("callback] Response status code:", response.status_code)
-        print("callback] Response content:", response.text)
+        payload = {"driver_id": 0, "user_id": 123, "order_id": "asd", "location": {"lat": 23.90, "lon": 90.23}}
+
+        # {"driver_id": 723, "user_id": 256, "order_id": "e27wbw", "location": {"lat": 23.90, "lon": 90.23}},
+        #####################################################
+        with self.client.post(
+            "http://localhost:4200/v1/callback",
+            # headers,
+            json=payload,
+            catch_response=True
+        ) as response:
+            print("[/callback] Response status code: '%d'" % response.status_code)
+            print("[/callback] Response test: '%s'" % response.text)
+            print("[/callback] Response content: '%s'" % response.content)
+            if response.text != "":
+                response.failure("[/callback] Expect '', Got " + response.text)
+            else:
+                print("[/callback] hurray! success")
+                response.success()
 
 class MyLocust(HttpLocust):
     task_set = MyTaskSet
